28219d478618c0bde13c7cdfe0ff236a2953f4a6
Farik Ametov <fikret.ametov@gmail.com>
2020-03-01 07:06:29 +0200

    fix-conf gulp4

--- gulpFile.js
+++ gulpFile.js
@@ -1,32 +1,41 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync');
-const rename = require('gulp-rename');
+const { src, dest } = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync");
+const rename = require("gulp-rename");
 
-gulp.task('sass', function() {
-    return gulp.src('app/src/sass/**/*.sass')
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('app/dist/css'))
-        .pipe(browserSync.reload({stream: true}))
+function style() {
+  return gulp
+    .src("app/src/sass/**/*.sass")
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("app/dest/css"))
+    .pipe(browserSync.reload({ stream: true }));
+}
+// gulp.task('sass', function() {
+//     return gulp.src('app/src/sass/**/*.sass')
+//         .pipe(sass({outputStyle: 'compressed'}))
+//         .pipe(rename({suffix: '.min'}))
+//         .pipe(gulp.dest('app/dest/css'))
+//         .pipe(browserSync.reload({stream: true}))
+// });
+
+gulp.task("html", function() {
+  return gulp.src("app/**/*.html").pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('html', function() {
-    return gulp.src('app/**/*.html')
-    .pipe(browserSync.reload({stream: true}))
+gulp.task("browser-sync", function() {
+  browserSync.init({
+    server: {
+      baseDir: "app/"
+    }
+  });
 });
 
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "app/"
-        }
-    });
+gulp.task("watch", function() {
+  gulp.watch("app/src/sass/**/*.sass", gulp.parallel("sass"));
+  gulp.watch("app/*.html", gulp.parallel("html"));
 });
 
-gulp.task('watch', function() {
-    gulp.watch('app/src/sass/**/*.sass', gulp.parallel('sass'))
-    gulp.watch('app/*.html', gulp.parallel('html'))
-});
+gulp.task("default", gulp.parallel("browser-sync", "watch"));
 
-gulp.task('default', gulp.parallel('browser-sync', 'watch'))
+exports.images = images;
